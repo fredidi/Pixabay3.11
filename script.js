5357bedb4e534316a85d841f54773440468b8cd1
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-02-07 23:42:42 +0100

    testing tags search

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 var API_KEY = '25560072-709e2be31011a4ece715ca1f6';
 
-
-
 document.querySelector("#input").addEventListener("keydown", (keyPressed) => {
     if (keyPressed.key == "Enter")
       apiRequest();
   });
   
-document.querySelector("#search").addEventListener("click", () => {
+document.querySelector("#blue").addEventListener("click", () => {
       apiRequest();
   });
   
   apiRequest = () => {
   
     document.querySelector("#img-wrapper").textContent = "";
-  
-    const url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10&colors='+validateClick+'';
+    
+   const url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10&colors='+blue.value+'';
   
     fetch(url)
   
@@ -36,22 +34,11 @@ document.querySelector("#search").addEventListener("click", () => {
     console.log('Images successfully loaded');
     for(let i = 0;i < data.hits.length;i++){
       let image = document.createElement("div");
-      image.className = "loadImg";
+      image.className = "load-Img";
       image.style.backgroundImage = "url("+data.hits[i].previewURL +")";
       image.addEventListener("click", function(){
         window.open(data.hits[i].pageURL, '_blank');
-       
-      
-    })
-    document.querySelector("#img-wrapper").appendChild(image);
+      })    
+    document.querySelector("#img-wrapper").appendChild(image); 
+    }
   }
-}
-
-validateClick = {
-  if (==clickAnycolor)
-
-  else if(== clickGrayscale)
-  else if(== clickTransparent)
-  else if(== clickRed)
-
-}
