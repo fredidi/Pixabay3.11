d3863e510ebc99ce50aa8c696859cc032ed3e867
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-02-09 19:17:17 +0100

    50% working next/prev btn

--- scripts/nextPreviousBtn.js
+++ scripts/nextPreviousBtn.js
@@ -2,20 +2,93 @@
 let APIKEY = '25560072-709e2be31011a4ece715ca1f6';
 let pageCount = 1;
 
-function changePageOnClick() {
-  let url = new URL('https://pixabay.com/api/?key='+APIKEY+'&q='+input.value+'&image_type=photo&per_page=10&page=${countPage}');
+function changePageOnClick(value) {
+  let url = new URL('https://pixabay.com/api/?key='+APIKEY+'&q=dog&image_type=photo&per_page=10&page=${countPage}');
   let search_params = url.searchParams;
+  if(value === 'next'){
+    console.log('next')
+    if(pageCount <= 10){
+      pageCount +=1;
+    search_params.set('page', pageCount)
+    let new_url = url.toString();
+    console.log(new_url);
 
-  pageCount +=1
-  search_params.set('page', pageCount)
-  let new_url = url.toString();
-  console.log(new_url);
-
-  fetch(new_url)
-
+    fetch(new_url)
+    .then(response => {
+      if (!response.ok) throw Error(response.statusText);
+        return response.json();
+     })
+    .then(data => {
+    
+      console.log('Images successfully loaded');
+      document.querySelector("#img-show").textContent = "";
+      for(let i = 0;i < data.hits.length;i++){
+          let container = document.createElement("div")
+          let image = document.createElement("img");
+          let userTag = document.createElement("h4");
   
- 
+          container.className = "content-wrapper"
+          image.className = "load-Img";
+          userTag.className = "load-User-Tag"; 
+  
+          image.style.backgroundImage = "url("+data.hits[i].previewURL +")";
+          image.addEventListener("click", function(){
+          window.open(data.hits[i].pageURL, '_blank');
+          })
+          userTag.setAttribute('style', 'white-space: pre;');
+          userTag.textContent = "Photographer: "+ data.hits[i].user +"\r\nTags: " + data.hits[i].tags;
+  
+          document.querySelector("#img-show").appendChild(container);
+          container.append(image);
+          container.append(userTag);
+      }
+    }
+    )}
+  }
+
+  else {
+    if(pageCount > 1)
+    console.log('prev')
+    pageCount -=1;
+    search_params.set('page', pageCount)
+    let new_url = url.toString();
+    console.log(new_url);
+
+    fetch(new_url)
+    .then(response => {
+      if (!response.ok) throw Error(response.statusText);
+        return response.json();
+     })
+    .then(data => {
+    
+      console.log('Images successfully loaded');
+      document.querySelector("#img-show").textContent = "";
+      for(let i = 0;i < data.hits.length;i++){
+          let container = document.createElement("div")
+          let image = document.createElement("img");
+          let userTag = document.createElement("h4");
+  
+          container.className = "content-wrapper"
+          image.className = "load-Img";
+          userTag.className = "load-User-Tag"; 
+  
+          image.style.backgroundImage = "url("+data.hits[i].previewURL +")";
+          image.addEventListener("click", function(){
+          window.open(data.hits[i].pageURL, '_blank');
+          })
+          userTag.setAttribute('style', 'white-space: pre;');
+          userTag.textContent = "Photographer: "+ data.hits[i].user +"\r\nTags: " + data.hits[i].tags;
+  
+          document.querySelector("#img-show").appendChild(container);
+          container.append(image);
+          container.append(userTag);
+      }
+    }
+  
+    )}
 }
+  
+
 
 
 
@@ -49,25 +122,3 @@ function changePageOnClick() {
 //     return url, page;
 //   })
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
