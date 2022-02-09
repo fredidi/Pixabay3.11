60b8562e6e597605a3ca83e79ef59ce47ad98ffa
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-02-09 15:31:30 +0100

    Update nextPreviousBtn.js
    next button works

--- scripts/nextPreviousBtn.js
+++ scripts/nextPreviousBtn.js
@@ -1,39 +1,54 @@
 
-var API_KEY = '25560072-709e2be31011a4ece715ca1f6';
-url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10&';
-page = '1';
+let APIKEY = '25560072-709e2be31011a4ece715ca1f6';
+let pageCount = 1;
+
+function changePageOnClick() {
+  let url = new URL('https://pixabay.com/api/?key='+APIKEY+'&q='+input.value+'&image_type=photo&per_page=10&page=${countPage}');
+  let search_params = url.searchParams;
+
+  pageCount +=1
+  search_params.set('page', pageCount)
+  let new_url = url.toString();
+  console.log(new_url);
+
+  fetch(new_url)
+
+  
+ 
+}
 
 
-function changePageOnClick(url, page) {
-    pageFetch(url,page);
- }
 
- function getParameter(parameterName) {
+// function changePageOnClick(url, page) {
+//     pageFetch(url,page);
+//  }
 
-    const url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10&';
+//  function getParameter(parameterName) {
 
-    let parameters = new URLSearchParams(url)
-    return parameters.get(parameterName);
+//     let url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10&';
 
- }
+//     let parameters = new URLSearchParams(url)
+//     return parameters.get(parameterName);
+
+//  }
 
 
-function pageFetch(url, page){
-  return fetch(`${url}&page=${page}`)
-  .then(res => res.json())
-  .then(newRes => {
+// function pageFetch(url, page){
+//   return fetch(`${url}&page=${page}`)
+//   .then(res => res.json())
+//   .then(newRes => {
 
     
-    console.log(newRes)
+//     console.log(newRes)
 
-    if(page <= 1){
-      page++
-    }
+//     if(page <= 1){
+//       page++
+//     }
 
-    console.log(url)
-    return url, page;
-  })
-}
+//     console.log(url)
+//     return url, page;
+//   })
+// }
 
 
 
