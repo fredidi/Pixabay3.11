63c9ab1fdd9df0649a81a7d92313617fb1e1e152
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-02-09 10:37:36 +0100

    Update script
    Finished new script for click on color from drop-down

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,40 @@
+//https://pixabay.com/api/?key=25560072-709e2be31011a4ece715ca1f6&q=dog&image_type=photo&per_page=10&page=2
+
 var API_KEY = '25560072-709e2be31011a4ece715ca1f6';
 
-//https://pixabay.com/api/?key=25560072-709e2be31011a4ece715ca1f6&q=dog&image_type=photo&per_page=10&colors=red
- //function changeColorOnClick(element) {
-  //   //look up window.location.href//   
-  // var url = new URL('https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10');
-  // var search_params = url.searchParams;
-  // // add "colors" parameter
-  // search_params.set('colors', 'red');
-  // url.search = search_params.toString();
-  // var new_url = url.toString();
-  // // output : http://demourl.com/path?id=100&topic=main
-  // console.log(new_url);
-// };
+  function changeColorOnClick(value) {
+  var url = new URL('https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10');
+  var search_params = url.searchParams;
 
+  search_params.set('colors', value);
+  var new_url = url.toString();
 
+  fetch(new_url)
 
-//Validate userinput on tags
-// const clickColor = document.getElementById("clickAnyColor");
-// clickColor.addEventListener("click", validate);
-// console.log(clickColor);
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+   })
 
-// const input = document.getElementById('clickAnyColor');
-// input.addEventListener('click', function(itsHappening) {
-//   if(itsHappening.key === 'click')
-//   apiRequest();
-// }
-// )
-// let validateClick = '';
-// if(input == "clickAnyColor")
-// apiRequest();
+   .then(data => {
+      console.log('Images successfully loaded');
+      document.querySelector("#img-wrapper").textContent = "";
+      for(let i = 0;i < data.hits.length;i++){
+      let image = document.createElement("div");
+      let tag = document.createElement("div");
+      image.className = "load-Img";
+      tag.className = "load-Tag";
+      image.style.backgroundImage = "url("+data.hits[i].previewURL +")";
+      tag.textContent="Tags: "+data.hits[i].tags;
+      image.addEventListener("click", function(){
+      window.open(data.hits[i].pageURL, '_blank');
+      })    
+    document.querySelector("#img-wrapper").appendChild(image);
+    document.querySelector("#img-wrapper").appendChild(tag);
+    }
+   })
+};
 
-//let url = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10';
 
 //--------------------------------------------------------
 document.querySelector("#input").addEventListener("keydown", (keyPressed) => {
@@ -71,18 +75,9 @@ document.querySelector("#input").addEventListener("keydown", (keyPressed) => {
       })    
     document.querySelector("#img-wrapper").appendChild(image); 
     }
+    
   }
-
-//   let sd = 'https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo'
-// document.querySelector("#img-wrapper").addEventListener("mouseover", function() {
-//         fetch(sd)
-//         .then(res =>{
-//           console.log(res)
-
-//         })
-        
-// })
-
+  
 
 // function showInfo(s) {
 // if(s.type == 'mouseover') {
