f03ba9a080c0cd96f8240eaa5b77058aa4de4795
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-02-11 19:00:02 +0100

    update script
    Removed trash-code
    Created OnClick-event for search with input and color  when click search-button/icon

--- scripts/script.js
+++ scripts/script.js
@@ -6,109 +6,74 @@ let API_KEY = '25560072-709e2be31011a4ece715ca1f6';
 
 
 
-  function changeColorOnClick(value) {
+function changeColorOnClick(selectedColor) {
+  if (selectedColor) {
+    console.log('will evaluate to true if value is not: null, "", undefine');
+    console.log('Color chosen: ' + selectedColor)
 
-    if( value ) {
-      console.log('will evaluate to true if value is not: null, "", undefine');
-      console.log('Color chosen: ' +value)
-      
-      document.body.addEventListener('keypress', (eventHandler) => {
-            if (eventHandler.key == "Enter") {
-                eventHandler.preventDefault();
-                try {
-                    if (input.value != '') {
-                        console.log('Log: '+input.value);
+    document.body.addEventListener('keypress', (eventHandler) => {
+      if (eventHandler.key == "Enter") {
+        eventHandler.preventDefault();
+        try {
+          if (input.value != '') {
+            console.log('Log: ' + input.value);
 
-                        let url = new URL('https://pixabay.com/api/?key='+API_KEY+'&image_type=photo&per_page=10');
-                        let search_params = url.searchParams;
-                        search_params.set('q', input.value);
-                        let new_url = url.toString();
+            let url = new URL('https://pixabay.com/api/?key=' + API_KEY + '&image_type=photo&per_page=10');
+            let search_params = url.searchParams;
+            search_params.set('q', input.value);
+            search_params.set('colors', selectedColor);
+            let new_url = url.toString();
 
-                        console.log('New Url: ' +new_url)
-                        
-                        fetchURL(new_url);
-                    }
-                    else{
-                        console.log('Empty input');
-                    }
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-          })
-      
-      // document.querySelector("#input").addEventListener("keydown", (keyPressed) => {
-      //   if (keyPressed.key == "Enter")
-      //   {
-      //     keyPressed.preventDefault();
-      //     console.log('Key pressed: ' + keyPressed.key);
-      //     console.log('Enter pressed: ' + value)
-      //     console.log('Input value on enter press: ' + input.value);
-      //   }
-        
-      // });
-      
-          
-      
-      // else{
-      //   console.log('Brad Pitt says: Life goes on')
-      // }
-      // color = value;
-      // // console.log(color.value)
-      // if(color === isNull){
-      //   console.log('null')
-      //   // let test = document.getElementById("#any-color")
-      //   // test.defaultValue = 'any-color';
-      //   // console.log(test)
-      // }
-      // else{
-      //   console.log('fel')
-      //   // clickSearchButton(color);
-      // }
-    // let newColor = value;
-    //   console.log(newColor);
-    //   let url = new URL('https://pixabay.com/api/?key='+API_KEY+'&q='+input.value+'&image_type=photo&per_page=10');
-    // let search_params = url.searchParams;
-    // search_params.set('colors', value);
-    // let new_url = url.toString();
-    // if(search_params == value) {
+            console.log('New Url: ' + new_url)
 
-    // }
+            fetchURL(new_url);
+          }
+          else {
+            console.log('Empty input');
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    })
+  }
+  if (selectedColor) {
+    document.querySelector("#search-button").addEventListener('click', (searchClick) => {
+      let teest = selectedColor
+      console.log('teest color on click ' + teest)
+      if (input.value != '') {
+        console.log('Log: ' + input.value);
+
+        let url = new URL('https://pixabay.com/api/?key=' + API_KEY + '&image_type=photo&per_page=10');
+        let search_params = url.searchParams;
+        search_params.set('q', input.value);
+        search_params.set('colors', selectedColor);
+        let new_url = url.toString();
+
+        console.log('New Url: ' + new_url)
+
+        fetchURL(new_url);
+      }
+      else {
+        console.log('Empty input');
+      }
+    })
   }
 }
 
-  // .then(response => {
-  //   if (!response.ok) throw Error(response.statusText);
-  //     return response.json();
-  //  })
 
-  //  .then(data => {
-  //     console.log('Images successfully loaded');
-  //     document.querySelector("#img-show").textContent = "";
-  //     for(let i = 0;i < data.hits.length;i++){
-  //         let container = document.createElement("div")
-  //         let image = document.createElement("img");
-  //         let userTag = document.createElement("h4");
+ 
+  
+  
 
-  //         container.className = "content-wrapper"
-  //         image.className = "load-Img";
-  //         userTag.className = "load-User-Tag"; 
+  // let clickColorSearch = document.querySelector(newColor)
+  // console.log(clickColorSearch.value)
 
-  //         image.style.backgroundImage = "url("+data.hits[i].previewURL +")";
-  //         image.addEventListener("click", function(){
-  //         window.open(data.hits[i].pageURL, '_blank');
-  //         })
-  //         userTag.setAttribute('style', 'white-space: pre;');
-  //         userTag.textContent = "Photographer: "+ data.hits[i].user +"\r\nTags: " + data.hits[i].tags;
+  // console.log(color);
+  
 
-  //         document.querySelector("#img-show").appendChild(container);
-  //         container.append(image);
-  //         container.append(userTag);
-  //     }
-      
 
-  //  })
-  // }
+
 
 
 
@@ -178,6 +143,7 @@ const fetchURL = async (new_url) => {
 
 const loadImages = async (data) => {
   for(let i = 0; i < data.hits.length; i++){
+    document.querySelector("#img-show").textContent = ''
     let container = document.createElement("div")
     let image = document.createElement("img");
     let userTag = document.createElement("h4");
